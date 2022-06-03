2411366e8b647d52b0bc9fd4a9d94f859a9d462a
Joshthefullstack <ibiamjoshua20@gmail.com>
2022-06-03 05:20:41 -0700

    submission

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,9 +5,9 @@
 
 /* betty style doc for function main goes there */
 /**
-*main - entry point;
+* main - entry point;
 *
-*return: Always 0 (success)
+* Return: Always 0 (success)
 */
 int main(void)
 {
