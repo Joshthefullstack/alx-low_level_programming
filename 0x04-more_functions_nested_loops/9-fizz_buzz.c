5a035de90089012cbe38bdf56fddf156fe7cbe28
Joshthefullstack <ibiamjoshua20@gmail.com>
2022-06-09 13:10:13 -0700

    nil

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,10 +12,13 @@ int main(void)
 	{
 		if (i % 15 == 0)
 			printf("FizzBuzz");
-		if (i % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
-		if (i % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
+		else
+			printf(" %d", i);
 	}
 	putchar('\n');
+	return (0);
 }
