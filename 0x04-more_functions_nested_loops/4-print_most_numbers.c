aadb97cc88a664c72177c220a91d0da9dbfc0f8d
Joshthefullstack <ibiamjoshua20@gmail.com>
2022-06-09 11:08:38 -0700

    nil

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 
 /**
- *
  * print_most_numbers - print most numbers 0-9 except two and four.
- *
- *Return: Return 0
+ * (neither 2 nor 4).
+ *Return: no return.
  */
 void print_most_numbers(void)
 {
