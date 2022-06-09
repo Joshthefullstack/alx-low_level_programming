5e8e11daa128522a9c6cf4c71fc54e46004367e8
Joshthefullstack <ibiamjoshua20@gmail.com>
2022-06-09 12:20:54 -0700

    nil

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,7 +3,7 @@
 
 /**
  * print_line - prints straight line n times.
- * @\n: times straight line is printed
+ * @n: times straight line is printed
  * Return: no return.
  */
 void print_line(int n)
