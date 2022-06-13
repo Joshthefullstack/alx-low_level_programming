ac4958f686af4804fcf8ea994fbcc0296b710a73
Joshthefullstack <ibiamjoshua20@gmail.com>
2022-06-13 08:18:28 -0700

    nil

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * a function that calculates the length of a string *s
+ * using strlen function
+ * Return: return 0
+ */
+void _strlen(char *s)
+{
+	size_t strlen(const char *s)
+}
