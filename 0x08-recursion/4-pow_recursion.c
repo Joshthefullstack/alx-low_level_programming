236cdf992686ece0b4e1cdd5b593ce061675733d
Joshthefullstack <ibiamjoshua20@gmail.com>
2022-06-22 16:08:02 -0700

    nil

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,8 +2,8 @@
 
 /**
  *  _pow_recursion - return the value of x raised to the power of y
- * @x - base.
- * @y - exponent
+ * @x: base.
+ * @y: exponent
  * Return: returns value of x raised to the power of y
  */
 int _pow_recursion(int x, int y)
