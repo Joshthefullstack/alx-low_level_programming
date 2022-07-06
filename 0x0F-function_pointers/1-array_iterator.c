169dc9747a28d51c805b5e7bf8f4080b6d2a8be2
Joshthefullstack <ibiamjoshua20@gmail.com>
2022-07-06 08:48:33 -0700

    nil

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,11 @@
 #include "function_pointers.h"
 
 /**
- * array-iterator: executs a function as a parameter on each element of an array.
+ * array-iterator - executs a function as a parameter on each element of an array.
  * @size: size of array
  * @i: iteration counter
  * @action: function pointer
+ *
  * Return: no return
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
