ca0bb58b5c52bed599962d7cc117b1faf0b1cf28
Joshthefullstack <ibiamjoshua20@gmail.com>
2022-07-06 09:26:28 -0700

    nil

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,14 +2,14 @@
 
 /**
  * int_index - searches for an integer
- * @arra: input integer array.
+ * @array: input integer array.
  * @size: size of the array
  * @cmp: pointer to the function to be used
  * to compare values.
  *
  * Return: the first element for which the cmp
- *function does not return 0. If elements matches.
- *return -1. If size <= 0, return 1.
+ * function does not return 0. If elements matches.
+ * return -1. If size <= 0, return 1.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
